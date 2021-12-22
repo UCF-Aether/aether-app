7bbacf280639f9dda3fb0d6419ef1f286cd31b48
Paul Wood <pmwood@pm.me>
2021-12-22 12:44:49 -0500

    Added certs for site

--- cdk/lib/frontend/static-site.ts
+++ cdk/lib/frontend/static-site.ts
@@ -1,11 +1,14 @@
-import { RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
 import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import { Construct } from "constructs";
+import { Aws, RemovalPolicy, CfnOutput, Stack } from 'aws-cdk-lib';
+import { ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export interface StaticSiteProps {
   domainName: string;
@@ -13,7 +16,7 @@ export interface StaticSiteProps {
 }
 
 export class StaticSite extends Construct {
-  constructor(scope: Construct, id: string, props: StaticSiteProps) {
+  constructor(scope: Stack, id: string, props: StaticSiteProps) {
     super(scope, id);
 
     const siteDomain = props.domainName;
@@ -51,6 +54,36 @@ export class StaticSite extends Construct {
       principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
     }));
     new CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
+
+    // TLS certificate
+    const certificateArn = new acm.DnsValidatedCertificate(this, 'SiteCertificate', {
+      domainName: siteDomain,
+      hostedZone: zone,
+      region: 'us-east-1', // Cloudfront only checks this region for certificates.
+    }).certificateArn;
+    new CfnOutput(this, 'Certificate', { value: certificateArn });
+
+    // Specifies you want viewers to use HTTPS & TLS v1.1 to request your objects
+    const viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
+      {
+        env: {
+          region: Aws.REGION,
+          account: Aws.ACCOUNT_ID
+        },
+        node: this.node,
+        stack: scope,
+        metricDaysToExpiry: () =>
+          new cloudwatch.Metric({
+            namespace: "TLS Viewer Certificate Validity",
+            metricName: "TLS Viewer Certificate Expired",
+          }),
+      } as ICertificate,
+      {
+        sslMethod: cloudfront.SSLMethod.SNI,
+        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+        aliases: [siteDomain]
+      }
+    )
 
     // CloudFront distribution
     const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
