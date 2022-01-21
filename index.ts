bd80beb4993818198c04b26c72cad130737bf1c2
Paul Wood <pmwood@pm.me>
2022-01-21 11:01:17 -0500

    Updated scripts and added dummy test

--- index.ts
+++ index.ts
@@ -15,6 +15,20 @@ export default function main(app: sst.App) {
   const siteDomain = stageConfig?.domain || undefined;
   const apiDomain = siteDomain ? `api.${siteDomain}` : undefined;
 
+  if (siteDomain) {
+    console.log(`Using domain: ${siteDomain}`);
+  }
+  else {
+    console.log("Using cloudfront domain");
+  }
+
+  if (apiDomain) {
+    console.log(`Using api domain: ${apiDomain}`);
+  }
+  else {
+    console.log("Using cloudfront api domain");
+  }
+
   new WebappStack(app, "Webapp", {
     domain: siteDomain,
     certificateArn: stageConfig ? config.dnsCertificates[stageConfig.domain] : undefined,
