d0a09444414bc6b8686569c9811361ab2ea1a2dc
Paul Wood <pmwood@pm.me>
2022-01-22 23:33:25 -0500

    Initial db stack
    - Might make it so only certain accounts can create dbs
      (infra.config.json), and specify public/private accessability

---
+++ infra/stacks/Database.ts
@@ -0,0 +1,32 @@
+import * as sst from "@serverless-stack/resources";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as logs from "aws-cdk-lib/aws-logs";
+
+export interface DatabaseStackProps extends sst.StackProps {
+
+}
+
+export class DatabaseStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: DatabaseStackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "VPC");
+
+    const dbInst = new rds.DatabaseInstance(this, "PostgresQLInst", {
+      engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_13_4 }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      cloudwatchLogsRetention: logs.RetentionDays.ONE_WEEK,
+      databaseName: "AetherDB",
+      allocatedStorage: 20,
+      publiclyAccessible: true,
+    });
+  }
+}
