1a27847457363d535cec4c1984842f7bcdec2bc8
Paul Wood <pmwood@pm.me>
2022-01-31 11:01:27 -0500

    Iot stack

--- index.ts
+++ index.ts
@@ -2,9 +2,13 @@ import * as sst from "@serverless-stack/resources";
 import { InfraConfig, StageConfig } from "./infra/util/InfraConfig";
 import { WebappStack } from "./infra/stacks/Webapp";
 import { ApiStack } from "./infra/stacks/Api";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 import * as _config from "./infra.config.json";
 import { DatabaseStack } from "./infra/stacks/Database";
+import { IotStack } from "./infra/stacks/Iot";
+import { VpcStack } from "./infra/stacks/Vpc";
+import { FunctionProps } from "@serverless-stack/resources";
 const config = _config as InfraConfig;
 
 export default function main(app: sst.App) {
@@ -30,6 +34,19 @@ export default function main(app: sst.App) {
     console.log("Using cloudfront api domain");
   }
 
+  const vpcStack = new VpcStack(app, "Vpc");
+
+  app.setDefaultFunctionProps((stack) => {
+    const appVpc = vpcStack.vpc;
+
+    return {
+      runtime: Runtime.NODEJS_14_X,
+      vpc: appVpc,
+      vpcSubnets: { subnets: appVpc.privateSubnets },
+      securityGroups: [vpcStack.sgs.lambda],
+    }
+  });
+
   new WebappStack(app, "Webapp", {
     domain: siteDomain,
     certificateArn: stageConfig ? config.dnsCertificates[siteDomain!] : undefined,
@@ -40,5 +57,9 @@ export default function main(app: sst.App) {
     certificateArn: stageConfig ? config.dnsCertificates[apiDomain!] : undefined,
   });
 
-  new DatabaseStack(app, "Database");
+  // Using supabase - plugin other postgres rds // new DatabaseStack(app, "Database");
+
+  new IotStack(app, "IoT", {
+
+  });
 }
---
+++ infra/stacks/Vpc.ts
@@ -0,0 +1,24 @@
+import * as sst from "@serverless-stack/resources";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
+export interface VpcStackProps extends sst.StackProps {
+
+}
+
+export class VpcStack extends sst.Stack {
+  vpc: ec2.Vpc;
+  sgs: {
+    lambda: ec2.SecurityGroup;
+  }
+
+  constructor(scope: sst.App, id: string, props?: VpcStackProps) {
+    super(scope, id, props);
+
+    this.vpc = new ec2.Vpc(this, "AetherVPC",);
+
+    this.sgs.lambda = new ec2.SecurityGroup(this, "LambdaSG", {
+      vpc: this.vpc,
+      description: "Security group for outbound lambda traffic",
+    });
+  }
+}
