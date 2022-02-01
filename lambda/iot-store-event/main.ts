17aeecd0151316cbe6eae24a53cf9c9dc0797d5a
Paul Wood <pmwood@pm.me>
2022-01-31 20:05:45 -0500

    Testing accessing db from lambda

--- lambda/iot-store-event/main.ts
+++ lambda/iot-store-event/main.ts
@@ -1,11 +1,27 @@
 import { Handler } from "aws-lambda";
+import { Client } from "pg";
 
-export const main: Handler = async function(event, context) {
+// Expects PG env vars
+const client = new Client();
+
+export const handler: Handler = async function(event, context) {
   console.log(event);
+  
   var gateways = event.WirelessMetadata.LoRaWAN.Gateways;
   gateways.forEach(function(gateway: object) {
       console.log(gateway);
   });
 
   console.log(`Payload: ${event.PayloadData}`);
+
+  console.log("Connecting to db");
+  await client.connect();
+  client
+    .query("SELECT NOW() as now")
+    .then(res => console.log(JSON.stringify(res)))
+    .catch(e => console.log(JSON.stringify(e)));
+  
+  return {
+    "javascript": "is making me question my sanity",
+  };
 }
