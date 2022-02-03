c6cc743ca6dcd0387ef86e13e87dee3e502c45ac
Paul Wood <pmwood@pm.me>
2022-02-02 22:15:36 -0500

    Fixed pg client not closing connection
    The connection technically ended successfully. Supabase also uses
    transactional pooling (pg-bouncer). But, the issue is that node-postgres
    saved some connection info between lambda invocations since the client
    was declared globally (not in the handler).

--- lambda/iot-store-event/main.ts
+++ lambda/iot-store-event/main.ts
@@ -2,39 +2,39 @@ import { Handler } from "aws-lambda";
 import { Client } from "pg";
 
 // Expects PG env vars
-const client = new Client();
 
 const query = `INSERT INTO devmsgs(time, rcvd, deveui, gweui, payload) VALUES (NOW(), $1, $2, $3, $4);`;
 
-export const handler: Handler = async function(event, context) {
+export const handler: Handler = async function (event, context) {
+  const client = new Client();
   console.log(event);
-  
-  const loraMeta = event.WirelessMetadata.LoRaWAN; 
+
+  const loraMeta = event.WirelessMetadata.LoRaWAN;
   const gateways = event.WirelessMetadata.LoRaWAN.Gateways;
-  gateways.forEach(function(gateway: object) {
-    console.log(gateway);
-  });
 
   const payload = event.PayloadData;
-  console.log(`Payload: ${payload}`);
   const deveui = loraMeta.DevEui;
   // There should really only be one...right?
   const gweui = gateways[0].GatewayEui;
   const timestamp = loraMeta.Timestamp;
 
   console.log("Connecting to db");
-  await client.connect().catch(e => console.log(e));
-  client
-    .query("SELECT NOW() as now")
-    .then(res => console.log(JSON.stringify(res)))
-    .catch(e => console.log(JSON.stringify(e)));
+  let res;
 
-  console.log("Inserting event data into db");
-  const res = await client.query(query, [timestamp, deveui, gweui, payload])
-  console.log(res);
-  
+  try {
+    await client.connect();
+  } catch (e) {
+    return { message: `Error connecting to db: ${e}`, status: 1 };
+  }
+
+  try {
+    await client.query(query, [timestamp, deveui, gweui, payload]);
+    res = { message: "Query successful!", status: 0 };
+  } catch (e) {
+    res = { message: `Error performing query: ${e}`, status: 2 };
+  }
+
   await client.end();
-  return {
-    "javascript": "is making me question my sanity",
-  };
-}
+
+  return res;
+};
