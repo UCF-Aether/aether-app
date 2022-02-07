53fb2dae1639193d7ac76cc5a8c489b1d6e707cf
Paul Wood <pmwood@pm.me>
2022-02-07 08:16:19 -0500

    Can deploy with fargate but not ec2?

--- infra/stacks/Api.ts
+++ infra/stacks/Api.ts
@@ -1,3 +1,4 @@
+import * as cdk from "aws-cdk-lib";
 import * as sst from "@serverless-stack/resources";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
@@ -14,7 +15,7 @@ export interface ApiStackProps extends sst.StackProps {
 
 export class ApiStack extends sst.Stack {
   cluster: ecs.Cluster;
-  service: ecsPatterns.ApplicationLoadBalancedEc2Service;
+  service: ecsPatterns.ApplicationLoadBalancedFargateService;
 
   constructor(scope: sst.App, id: string, props: ApiStackProps) {
     super (scope, id, props);
@@ -42,26 +43,31 @@ export class ApiStack extends sst.Stack {
       desiredCapacity: 1,
     });
 
-    this.service = new ecsPatterns.ApplicationLoadBalancedEc2Service(this, "ApiService", {
+    console.log(`cert: ${certificate}`);
+    console.log(`domain: ${props.domain}`);
+    console.log(`hz: ${domainZone}`);
+
+    this.service = new ecsPatterns.ApplicationLoadBalancedFargateService(this, "PostgraphileApiService", {
       cluster: this.cluster,
-      certificate,
       memoryLimitMiB: 1024,
-      // Valid values, which determines your range of valid values for the memory parameter:
-      //  256 (.25 vCPU) - Available memory values: 0.5GB, 1GB, 2GB
-      //  512 (.5 vCPU) - Available memory values: 1GB, 2GB, 3GB, 4GB
-      //  1024 (1 vCPU) - Available memory values: 2GB, 3GB, 4GB, 5GB, 6GB, 7GB, 8GB
-      //  2048 (2 vCPU) - Available memory values: Between 4GB and 16GB in 1GB increments
-      //  4096 (4 vCPU) - Available memory values: Between 8GB and 30GB in 1GB increments
-      cpu: 256,
       desiredCount: 1,
+      cpu: 512,
+      circuitBreaker: { rollback: true },
+      serviceName: "PostgraphileApi",
       domainName: props.domain,
       domainZone,
+      certificate,
+      redirectHTTP: certificate != null || certificate != undefined,
       taskImageOptions: {
         image: ecs.ContainerImage.fromAsset("api/"),
+        containerPort:80,
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
-        }
-      }
-    })
+          APP_STAGE: scope.stage.toUpperCase(),
+        },
+      },
+      publicLoadBalancer: true,
+      assignPublicIp: true,
+    });
   }
 }
