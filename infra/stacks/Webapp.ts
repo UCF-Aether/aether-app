783dc9878283bf7c0ebc01e93d4dc6e0f92aec90
Paul Wood <pmwood@pm.me>
2022-02-07 18:34:35 -0500

    Updated env vars and fixed mixed-content error

--- infra/stacks/Webapp.ts
+++ infra/stacks/Webapp.ts
@@ -1,10 +1,12 @@
 import { StaticSite, StaticSiteProps } from "@serverless-stack/resources";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as sst from "@serverless-stack/resources";
 import { RemovalPolicy } from "aws-cdk-lib";
 import { CfnOutput } from "aws-cdk-lib";
 
 export interface WebappStackProps extends sst.StackProps {
+  graphqlUrl: string;
   domain?: string;
   certificateArn?: string;
 }
@@ -20,6 +22,9 @@ export class WebappStack extends sst.Stack {
       path,
       buildOutput: "build",
       buildCommand: `pnpm build --filter ${path}`,
+      environment: {
+        REACT_APP_GRAPHQL_URL: props.graphqlUrl,
+      },
       s3Bucket: {
         removalPolicy: scope.stage === "prod" ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
         autoDeleteObjects: scope.stage !== "prod",
@@ -33,6 +38,19 @@ export class WebappStack extends sst.Stack {
           domainName: props.domain,
           domainAlias: `www.${props.domain}`,
           certificate: Certificate.fromCertificateArn(this, "SiteCertificate", props.certificateArn!),
+        }
+      }
+    }
+    else {
+      // Don't have a certificate for ELB (elastic load balancer) used for in the API stack. Allow
+      // both HTTP and HTTPS for non-prod environments (prod has domain) or else there will be
+      // mixed-content errors due to the cloudfront distribution being (defaults to) redirect-to-HTTPS.
+      siteProps = {
+        ...siteProps,
+        cfDistribution: {
+          defaultBehavior: {
+            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL,
+          }
         }
       }
     }
