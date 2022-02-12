b26630bbba42b3490f07b8f1265c34c0b5930daf
Paul Wood <pmwood@pm.me>
2022-02-12 14:38:59 -0500

    fix: vpc creating 3 NATs instead of 1
    TODO: look into managing a NAT on EC2 to utilize the free tier

--- infra/stacks/Vpc.ts
+++ infra/stacks/Vpc.ts
@@ -9,23 +9,32 @@ export class VpcStack extends sst.Stack {
   vpc: ec2.Vpc;
   sgs: {
     lambda: ec2.SecurityGroup;
-    api: ec2.SecurityGroup;
+    // api: ec2.SecurityGroup;
   }
 
   constructor(scope: sst.App, id: string, props?: VpcStackProps) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, "AetherVPC",);
+    this.vpc = new ec2.Vpc(this, "AetherVPC", {
+      natGateways: 1,
+      // subnetConfiguration: [
+      //   {
+      //     cidrMask: 24,
+      //     name: 'application',
+      //     subnetType: ec2.SubnetType.PUBLIC,
+      //   },
+      // ]
+    });
 
     this.sgs = {
       lambda: new ec2.SecurityGroup(this, "LambdaSG", {
         vpc: this.vpc,
         description: "Security group for outbound lambda traffic",
       }),
-      api: new ec2.SecurityGroup(this, "ApiEc2SG", {
-        vpc: this.vpc,
-        description: "Security group for the API EC2 instance",
-      }),
+      // api: new ec2.SecurityGroup(this, "ApiEc2SG", {
+      //   vpc: this.vpc,
+      //   description: "Security group for the API EC2 instance",
+      // }),
     };
   }
 }
