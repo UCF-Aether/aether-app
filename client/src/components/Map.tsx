cf55bda9c17eafe4c67726b3f1583d32d9b3d095
Paul Wood <pmwood@pm.me>
2022-02-22 15:26:52 -0500

    Added non-functional map search bar

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,11 +1,24 @@
-import { useEffect } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
-import Box from "@mui/material/Box";
+import { TextField, Box, Paper, Card } from "@mui/material";
+import { createRef, forwardRef, useEffect } from "react";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 let map: google.maps.Map;
 
-export function DataMap() {
+// eslint-disable-next-line react/display-name
+const SearchBar = forwardRef<HTMLDivElement>((props: any, ref) => (
+  <div ref={ref}>
+    <Card sx={{ m: 1, width: 400 }}>
+      <TextField id='maps-search-bar' label='Search' variant='outlined' sx={{ width: '100%', height: '100%' }}/>
+    </Card>
+  </div>
+));
+
+function Map() {
+  const searchRef = createRef<HTMLDivElement>();
+
+  const searchBar = <SearchBar ref={searchRef} />
+
   useEffect(() => {
     const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     if (!apiKey) throw new Error("REACT_APP_GOOGLE_MAPS_API_KEY not defined");
@@ -19,11 +32,24 @@ export function DataMap() {
       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 28.603053295127882, lng: -81.19995194984672 },
         zoom: 12,
+        streetViewControl: false,
+        mapTypeControl: false,
       });
+
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchRef.current);
     });
   });
 
   return (
-    <div id="map"></div>
+    <Box sx={{ height: '100%' }}>
+      <div id="map"></div>
+      {searchBar}
+    </Box>
+  );
+}
+
+export function DataMap() {
+  return (
+    <Map />
   );
 }
