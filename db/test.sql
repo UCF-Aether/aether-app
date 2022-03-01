51add5978c34be371028d2670a0d726fa271d6c8
Paul Wood <pmwood@pm.me>
2022-03-01 15:13:11 -0500

    db: better test data generation

--- db/test.sql
+++ db/test.sql
@@ -1,30 +1,48 @@
-select * from auth.users;
-select * from public.profile;
-insert into auth.users (id, role, email) values ('00000000-0000-0000-0000-000000000000', 'authenticated', 'test@test.test');
-select * from device;
-insert into device (dev_eui, profile_id, name, aws_device_id, bme_config, bme_state) VALUES
-(
- '0000000000000000', '00000000-0000-0000-0000-000000000000', 'Test device', '0', '{}'::json, '{}'::json
-);
-select * from device;
+create or replace function create_random_devices(for_profile uuid, num integer)
+  returns void
+  language sql
+as
+$$
+insert into device (profile_id, name, dev_eui, aws_device_id)
+select for_profile            as profile_id,
+       'Device-' || eui::text as name,
+       eui                    as dev_eui,
+       eui                    as aws_device_id
+from (select generate_series(1, num), generate_eui() as eui) as ids;
 
-insert into location (loc_geog) values
-(st_makepoint(0.0, 0.0)::geography);
+$$;
 
-update device_meta
-  set  loc_id = (select loc_id from location fetch first row only),
-       loc_method = 'MANUAL';
-select * from device_meta;
+create or replace function create_random_readings(for_profile uuid, loc geography(point), radius float, num integer)
+  returns void
+  language sql
+as
+$$
+insert into reading (device_id, loc_id, sensor_chan_id, taken_at, val)
+select device_id,
+       generate_random_location(st_x(loc::geometry), st_y(loc::geometry), radius),
+       (select sensor_chan_id from sensor_chan where sensor_chan.name = 'AQI'),
+       now(),
+       69
+from generate_series(1, num)
+       cross join lateral (select * from device where profile_id = for_profile) as d;
+$$;
 
-insert into reading (device_id, sensor_chan_id, taken_at, received_at, val) VALUES
-(
- (select device_id from device fetch first row only),
- (select sensor_chan_id from sensor_chan where sensor_chan.name = 'AQI'),
- now(), now(), 69.420
-);
+create or replace function create_random_test_data()
+  returns trigger
+  language plpgsql
+as
+$$
+begin
+  perform create_random_devices(new.profile_id, 100);
+  perform create_random_readings(new.profile_id, st_makepoint(28.611644, -81.209604), 5, 100);
+  return new;
+end;
+$$;
 
-select * from reading;
-select * from sensor_chan;
-select * from get_device_location(2);
-select * from new_reading('0000000000000000', 'O3', now(), 420.69);
-select  readings_within();
+select create_random_test_data();
+
+create trigger create_test_data_trigger
+  after insert
+  on profile
+  for each row
+  execute procedure create_random_test_data();
