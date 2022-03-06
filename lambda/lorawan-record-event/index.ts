2723e499ef86e222b9f40f96d18dc132b6c31300
Paul Wood <pmwood@pm.me>
2022-03-06 13:29:55 -0500

    infra(lambda/lorawan-record-event): changed supabase key env var name

--- lambda/lorawan-record-event/index.ts
+++ lambda/lorawan-record-event/index.ts
@@ -127,7 +127,7 @@ async function record(supabase: SupabaseClient, event: LorawanEvent) {
 export const handler = async function(event: LorawanEvent) {
   const supabase = createClient(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
   );
 
   await record(supabase, event);
