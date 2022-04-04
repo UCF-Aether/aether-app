5295e6f8a32acdc6bd5ee3750ca54b6d1fbebbda
Paul Wood <pmwood@pm.me>
2022-04-04 14:06:55 -0400

    Optimized optional function parameter where clauses
    - Formatting
    - Used (<param> is null or <cond with param>) instead of always filtering
      with default values

--- db/supabase/migrations/20220404003328_binned_reading_search_fun.sql
+++ db/supabase/migrations/20220404003328_binned_reading_search_fun.sql
@@ -1,61 +1,44 @@
 create type simple_reading as
 (
-    timestamp timestamp,
-    device_id int,
-    chan_id   int,
-    val       double precision
+  timestamp timestamp,
+  device_id int,
+  chan      text,
+  val       double precision
 );
 
 create type binned_reading as
 (
-    loc_id   int,
-    lat      float,
-    lng      float,
-    readings simple_reading[]
+  loc_id   int,
+  lat      float,
+  lng      float,
+  readings simple_reading[]
 );
 
 create or replace function readings(
-    start timestamp default '2000-01-01',
-    lng_min float default -180,
-    lat_min float default -90,
-    lng_max float default 90,
-    lat_max float default 180,
-    chan text default null)
-    returns setof binned_reading as
+  start timestamp default null,
+  lng_min float default null,
+  lat_min float default null,
+  lng_max float default null,
+  lat_max float default null,
+  chan text default null)
+  returns setof binned_reading as
 $$
-declare
-    chan_id int;
-begin
-    if chan is not null then
-        select sensor_chan_id
-        from sensor_chan
-        where name = chan
-        into chan_id;
-    end if;
-
-    return query
-        select loc_id,
-               st_x(loc_geog::geometry) as lng,
-               st_y(loc_geog::geometry) as lat,
-               readings.readings_array  as readings
-        from location
-                 join (
-            select loc_id,
-                   array_agg(
-                           row (taken_at, device_id, sensor_chan_id, val)::simple_reading
-                           order by taken_at ASC) as readings_array
-            from reading
-            where taken_at >= start
-              and case
-                      when chan is not null then
-                          sensor_chan_id = chan_id
-                      else
-                          true
-                end
-            group by loc_id
-        ) as readings using (loc_id)
-        where loc_geog && st_makeenvelope(lng_min, lat_min, lng_max, lat_max, 4326);
-end;
-$$
-    language plpgsql stable
-                     parallel safe;
+select loc_id,
+       st_x(loc_geog::geometry) as lng,
+       st_y(loc_geog::geometry) as lat,
+       readings.readings_array  as readings
+from location
+       join (
+  select loc_id,
+         array_agg(
+             row (taken_at, device_id, sc.name, val)::simple_reading
+             order by taken_at) as readings_array
+  from reading rd
+         join sensor_chan sc on rd.sensor_chan_id = sc.sensor_chan_id
+  where (start is null or taken_at >= start)
+    and (chan is null or chan = sc.name)
+  group by loc_id
+) as readings using (loc_id)
+where (lng_min is null or loc_geog::geometry @ st_makeenvelope(lng_min, lat_min, lng_max, lat_max, 4326));
+$$ language sql stable
+                parallel safe;
