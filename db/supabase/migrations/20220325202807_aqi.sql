29da949b5e06129f61fd9768322ddcd53942f855
Paul Wood <pmwood@pm.me>
2022-04-04 14:03:52 -0400

    Renamed hourly_aqi, added hourly_aqi view
    - Formatting
    - Renamed hourly_aqi table to raw_hourly_aqi
    - Added hourly_aqi view (doesn't yet select midpoint over raw aqi)

--- db/supabase/migrations/20220325202807_aqi.sql
+++ db/supabase/migrations/20220325202807_aqi.sql
@@ -1,12 +1,12 @@
 create table hourly_reading_stats
 (
-  device_id int references device (device_id) not null,
-  loc_id    int references location (loc_id) not null,
+  device_id int references device (device_id)           not null,
+  loc_id    int references location (loc_id)            not null,
   chan_id   int references sensor_chan (sensor_chan_id) not null,
   hour      smallint    default extract(hour from now()),
   day       date        default date_trunc('day', now())::date,
   cur_time  timestamptz default now(),
-  sum       double precision not null,
+  sum       double precision                            not null,
   count     int         default 1,
   max       double precision,
   min       double precision,
@@ -21,25 +21,22 @@ create or replace function update_hourly_reading_stats()
 as
 $$
 begin
-  insert into
-    hourly_reading_stats
+  insert into hourly_reading_stats
     (device_id, loc_id, hour, day, chan_id, sum, max, min)
-  values
-    (new.device_id,
-     new.loc_id,
-     extract(hour from new.taken_at),
-     date_trunc('day', new.taken_at)::date,
-     new.sensor_chan_id,
-     new.val,
-     new.val,
-     new.val)
+  values (new.device_id,
+          new.loc_id,
+          extract(hour from new.taken_at),
+          date_trunc('day', new.taken_at)::date,
+          new.sensor_chan_id,
+          new.val,
+          new.val,
+          new.val)
   on conflict (loc_id, hour, day, chan_id) do update
-    set
-      cur_time = now(),
-      sum = excluded.sum + hourly_reading_stats.sum,
-      count = hourly_reading_stats.count + 1,
-      max = greatest(excluded.sum, hourly_reading_stats.max),
-      min = least(excluded.sum, hourly_reading_stats.min);
+    set cur_time = now(),
+        sum      = excluded.sum + hourly_reading_stats.sum,
+        count    = hourly_reading_stats.count + 1,
+        max      = greatest(excluded.sum, hourly_reading_stats.max),
+        min      = least(excluded.sum, hourly_reading_stats.min);
 
   return new;
 end;
@@ -61,52 +58,40 @@ create or replace function get_sensor_chan_id(chan text)
   language sql
 as
 $$
-select
-  sensor_chan_id
-from
-  sensor_chan
-where
-  sensor_chan.name = chan;
+select sensor_chan_id
+from sensor_chan
+where sensor_chan.name = chan;
 $$;
 
 create or replace view hourly_o3 as
-select
-  hour,
-  day,
-  chan_id,
-  avg,
-  device_id,
-  loc_id
-from
-  hourly_reading_stats
-where
-  chan_id = get_sensor_chan_id('O3');
+select hour,
+       day,
+       chan_id,
+       avg,
+       device_id,
+       loc_id
+from hourly_reading_stats
+where chan_id = get_sensor_chan_id('O3');
 
 create or replace view hourly_pm2_5 as
-select
-  hour,
-  day,
-  chan_id,
-  avg,
-  device_id,
-  loc_id
-from
-  hourly_reading_stats
-where
-  chan_id = get_sensor_chan_id('PM2_5');
+select hour,
+       day,
+       chan_id,
+       avg,
+       device_id,
+       loc_id
+from hourly_reading_stats
+where chan_id = get_sensor_chan_id('PM2_5');
 
 create or replace view hourly_pm10 as
-select
-  hour,
-  day,
-  chan_id,
-  avg,
-  device_id,
-  loc_id
-from
-  hourly_reading_stats
-where
-  chan_id = get_sensor_chan_id('PM10');
+select hour,
+       day,
+       chan_id,
+       avg,
+       device_id,
+       loc_id
+from hourly_reading_stats
+where chan_id = get_sensor_chan_id('PM10');
 
 
 create table pollutant
@@ -119,10 +104,10 @@ create table pollutant
 create table pollutant_subindex
 (
   pollutant_subindex_id int primary key generated always as identity,
-  pol                   text not null references pollutant (name),
-  timeframe_hours       int not null,
-  aqi_low               smallint not null,
-  aqi_high              smallint not null,
+  pol                   text             not null references pollutant (name),
+  timeframe_hours       int              not null,
+  aqi_low               smallint         not null,
+  aqi_high              smallint         not null,
   conc_low              double precision not null,
   conc_high             double precision not null,
   advisory_statement    text default '',
@@ -139,20 +124,17 @@ create or replace function get_pollutant_subindex(pollutant text, conc double pr
 as
 $$
 select *
-from
-  pollutant_subindex
-where
-    conc_low <= conc
-and conc_high >= conc
-and pol = pollutant
-and case
-      when for_timeframe_hours > 0 then
-        timeframe_hours = for_timeframe_hours
-      when pollutant = 'O3' then
-        timeframe_hours = 8
-      else true end
-order by
-  timeframe_hours ASC
+from pollutant_subindex
+where conc_low <= conc
+  and conc_high >= conc
+  and pol = pollutant
+  and case
+        when for_timeframe_hours > 0 then
+          timeframe_hours = for_timeframe_hours
+        when pollutant = 'O3' then
+          timeframe_hours = 8
+        else true end
+order by timeframe_hours ASC
 limit 1;
 $$;
 
@@ -164,26 +146,22 @@ as
 $$
 declare
   trunc_conc double precision;
-  aqi int;
+  aqi        int;
 begin
-  select
-    trunc(conc::numeric, pm.trunc_amt)
-  from
-    pollutant pm
-  where
-    pm.name = pollutant
+  select trunc(conc::numeric, pm.trunc_amt)
+  from pollutant pm
+  where pm.name = pollutant
   into
     trunc_conc;
 
-  select
-      case
-        when trunc_conc < (select min(conc_low) from pollutant_subindex where pol = pollutant) then
-          0
-        when trunc_conc > (select max(conc_high) from pollutant_subindex where pol = pollutant) then
-          500
-        else
-          aqi_low + (aqi_high - aqi_low) * (trunc_conc - conc_low) / (conc_high - conc_low)
-      end
+  select case
+           when trunc_conc < (select min(conc_low) from pollutant_subindex where pol = pollutant) then
+             0
+           when trunc_conc > (select max(conc_high) from pollutant_subindex where pol = pollutant) then
+             500
+           else
+               aqi_low + (aqi_high - aqi_low) * (trunc_conc - conc_low) / (conc_high - conc_low)
+           end
   from
     get_pollutant_subindex(pollutant, trunc_conc, for_timeframe_hours)
   into aqi;
@@ -193,16 +171,17 @@ end;
 $$;
 
 create type hourly_aqi_type as enum ('RAW', 'MIDPOINT', 'NOWCAST');
-create table hourly_aqi
+create table raw_hourly_aqi
 (
   hourly_aqi_id int generated always as identity,
-  device_id     int references device (device_id) not null,
-  loc_id        int references location (loc_id) not null,
+  device_id     int references device (device_id)            not null,
+  loc_id        int references location (loc_id)             not null,
   pollutant_id  smallint references pollutant (pollutant_id) not null,
-  hour          smallint not null,
-  day           date not null,
-  aqi           smallint not null,
-  type          hourly_aqi_type not null,
+  hour          smallint                                     not null,
+  day           date                                         not null,
+  aqi           smallint                                     not null,
+  type          hourly_aqi_type                              not null,
+  timestamp     timestamp generated always as ( day + interval '1hr' * hour) stored,
   unique (day, hour, pollutant_id, loc_id, device_id, type)
 );
 
@@ -212,52 +191,39 @@ create or replace function update_hourly_aqi()
 as
 $$
 declare
-  pol_id smallint;
+  pol_id   smallint;
   pol_name text;
-  new_aqi smallint;
+  new_aqi  smallint;
 begin
   if not new.chan_id in (
-    select
-      sensor_chan_id
-    from
-      sensor_chan sc
-    where
-      sc.name in ('O3', 'PM2_5', 'PM10', 'NO2', 'CO', 'SO2')) then
+    select sensor_chan_id
+    from sensor_chan sc
+    where sc.name in ('O3', 'PM2_5', 'PM10', 'NO2', 'CO', 'SO2')) then
     return new;
   end if;
 
-  select
-    name
-  from
-    pollutant p
-  where
-      p.name = (select sc.name from sensor_chan sc where sc.sensor_chan_id = new.chan_id)
+  select name
+  from pollutant p
+  where p.name = (select sc.name from sensor_chan sc where sc.sensor_chan_id = new.chan_id)
   into pol_name;
 
-  select
-    pollutant_id
-  from
-    pollutant p
-  where
-    p.name = pol_name
+  select pollutant_id
+  from pollutant p
+  where p.name = pol_name
   into pol_id;
 
   select conc_to_aqi(pol_name, new.avg) into new_aqi;
 
-  raise notice 'id=%', pol_id;
-  insert into
-    hourly_aqi (device_id, loc_id, pollutant_id, hour, day, aqi, type)
-  values
-    (new.device_id,
-     new.loc_id,
-     pol_id,
-     new.hour,
-     new.day,
-     new_aqi,
-     'RAW')
+  insert into raw_hourly_aqi (device_id, loc_id, pollutant_id, hour, day, aqi, type)
+  values (new.device_id,
+          new.loc_id,
+          pol_id,
+          new.hour,
+          new.day,
+          new_aqi,
+          'RAW')
   on conflict (day, hour, pollutant_id, loc_id, device_id, type) do update
-    set
-      aqi = new_aqi;
+    set aqi = new_aqi;
 
   return new;
 end;
@@ -275,51 +241,32 @@ create trigger update_hourly_aqi_trigger
   for each row
 execute procedure update_hourly_aqi();
 
+create or replace view hourly_aqi as
+select device_id, loc_id, pollutant_id, timestamp, type, max(aqi) as aqi
+from raw_hourly_aqi
+where case
+        when timestamp = date_trunc('hour', now()) then
+          type = 'NOWCAST'
+        else
+          type = 'RAW' -- TODO: It should somehow prefer MIDPOINT over RAW, if MIDPOINT exists
+        end
+group by device_id, loc_id, pollutant_id, timestamp, type
+order by timestamp;
 
--- type can be aqi_type or 'MAX'
-create or replace function get_aqi(loc int, at timestamp default now(), for_type text default 'MAX')
-  returns smallint
-  language sql
-as
-$$
-with
-  cur_hours_aqi as (
-    select *
-    from
-      hourly_aqi
-    where
-        loc_id = loc
-    and hour = extract(hour from at)
-    and day = date_trunc('day', at)
-  )
-select
-  max(aqi)
-from
-  cur_hours_aqi
-where
-  case
-    when for_type = 'MAX' then
-      true
-    else
-      type = for_type::hourly_aqi_type
-    end;
-$$;
-
-delete from pollutant;
-insert into
-  pollutant (name, trunc_amt)
-values
-  ('O3', 3),
-  ('PM2_5', 1),
-  ('PM10', 0),
-  ('CO', 1),
-  ('SO2', 0),
-  ('NO', 0);
+delete
+from pollutant;
+insert into pollutant (name, trunc_amt)
+values ('O3', 3),
+       ('PM2_5', 1),
+       ('PM10', 0),
+       ('CO', 1),
+       ('SO2', 0),
+       ('NO', 0);
 -- Values from Table 4 from "Technical Assistance Document for the Reporting of Daily Air Quality – the Air Quality
 -- Index (AQI)"
-delete from pollutant_subindex;
-insert into
-  pollutant_subindex (pol, timeframe_hours, aqi_low, aqi_high, conc_low, conc_high, advisory_statement)
+delete
+from pollutant_subindex;
+insert into pollutant_subindex (pol, timeframe_hours, aqi_low, aqi_high, conc_low, conc_high, advisory_statement)
 values
   -- O3 8 hour
   ('O3', 8, 0, 50, 0, 0.054,
