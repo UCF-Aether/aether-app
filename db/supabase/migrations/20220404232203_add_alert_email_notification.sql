5b57c0ec70708e7ab69f053ae539c18f1d073af0
Paul Wood <pmwood@pm.me>
2022-04-05 11:32:44 -0400

    Added null check on alert.post & alert.get_triggered

--- db/supabase/migrations/20220404232203_add_alert_email_notification.sql
+++ db/supabase/migrations/20220404232203_add_alert_email_notification.sql
@@ -10,46 +10,51 @@ create table alert.definition
 (
   definition_id int generated always as identity,
   profile_id uuid not null,
-  fid int not null,
-  source text,
+  fid int not null, -- foreign id - sensor_chan_id or pollutant_id
+  source text, -- reading (public.reading) or aqi (public.raw_hourly_aqi)
   trigger float
 );
 
-create or replace function alert.post(pid uuid, def_id int, value float)
-  returns void
+create or replace function alert.post(pid uuid, source text, trigger_val float, value float)
+  returns jsonb
   security definer
 as
 $$
 declare
   profile_email text;
-  sensor        text;
-  trig_val      float;
   ret           json;
 begin
   select email
-  from public.profile p
-  join auth.users u on u.id = p.profile_id
-  where profile_id = pid
+  from auth.users
+  where id = pid
   into profile_email;
 
-  select trigger_val, sc.name
-  from public.alert_def
-         join sensor_chan sc on sc.sensor_chan_id = alert_def.sensor_chan_id
-  where alert_def_id = def_id
-  into trig_val, sensor;
+  if profile_email is null then raise exception 'Profile % does not exist', pid; end if;
 
   select into ret
     send_email_message(
     jsonb_build_object(
       'sender', 'alerts@aethersensor.network',
       'recipient', profile_email,
-      'subject', 'Trigger alert for ' || sensor,
-      'html_body', 'Your trigger for '|| sensor || ' was activated at value='|| value ||', trigger=' || trig_val
+      'subject', 'Trigger alert for ' || source,
+      'html_body', 'Your trigger for '|| source || ' was activated at value='|| value ||', trigger=' || trigger_val
     ));
+
+  return ret;
 end;
 $$ language plpgsql;
 
 revoke execute on function alert.post from public;
+
+create or replace function alert.get_triggered (src text, foreign_id int, trig_val float)
+returns setof alert.definition as $$
+  select *
+  from alert.definition
+  where source = src and fid = foreign_id and trigger = trig_val;
+$$ language sql;
+
+
+select event.create_type('alert_triggered', false);
 
 -- I know this would probably be done better in a trigger, but I'm experimenting
 create or replace function handle_trigger_event(pid uuid, body jsonb, at timestamp)
@@ -57,7 +62,12 @@ returns void
 stable
 parallel safe as $$
 declare
+  alert_def_id int;
+  trig_val float;
+  source float;
 begin
-
+  alert_def_id := body ->> 'definition_id';
+  trig_val := body ->> 'trigger';
+  source := body ->> 'source';
 end;
 $$ language plpgsql;
