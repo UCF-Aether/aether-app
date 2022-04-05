d9569ef091d90126a4caa33c3a47e0bf999d132d
Paul Wood <pmwood@pm.me>
2022-04-04 23:20:50 -0400

    Removed unnecessarily email field on profile

--- db/supabase/migrations/20220404231953_add_profile_email.sql
+++
@@ -1,8 +0,0 @@
--- Add email to profile schema
-create extension if not exists citext;
-create domain email as citext
-  check ( value ~
-          '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
-
-alter table profile
-  add column email email;
