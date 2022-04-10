956d0132811f5b857390ff716d304f58a7ea9032
Paul Wood <pmwood@pm.me>
2022-04-10 01:24:38 -0400

    Updated gateways dashboard to use useGateways

--- client/src/pages/dashboard/Gateways.tsx
+++ client/src/pages/dashboard/Gateways.tsx
@@ -1,38 +1,34 @@
 import { Box, Button } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { useQuery } from "urql";
 import LinearProgress from '@mui/material/LinearProgress';
+import { DataGrid } from "@mui/x-data-grid";
+import { useMemo } from "react";
 import { BasePanel } from "../../components/panels/BasePanel";
-import { DevicesDocument, GatewaysDocument } from "../../generated/graphql";
+import { useGateways } from "../../hooks/gateways";
 
 const columns = [
   { field: "name", headerName: "Name", flex: 1 },
-  { field: "id", headerName: "Gateway EUI", flex: 1},
-  { field: "updatedAt", headerName: "Updated At", flex: 1},
+  { field: "gateway_eui", headerName: "Gateway EUI", flex: 1},
+  { field: "updated_at", headerName: "Updated At", flex: 1},
 ];
 
 export function Gateways() {
-  const [{ data, fetching, error }, getDevices] = useQuery({
-    query: GatewaysDocument,
-  });
-
-  const rows = data?.gateways?.nodes
-    .filter(dev => dev != null)
-    .map(gw => ({
-      id: gw.id,
-      name: gw.name,
-      updatedAt: gw.gatewayMeta.updatedAt ?? '-',
-    })) || [];
+  const { isLoading, isError, gateways } = useGateways();
+  const rows = useMemo(
+    () => (gateways ?? []).map(g => ({
+      ...g,
+      id: g.gateway_id,
+      updated_at: g.updated_at ?? '-',
+    })), 
+    [gateways]);
 
   return (
     <Box sx={{ height: '100vh', m: 2  }}>
-      <Button variant='outlined'>Add Gateway</Button>
+      <Button variant='outlined'>Add Device</Button>
       <BasePanel contentSx={{ height: '60vh', flexGrow: 1 }}>
         <DataGrid 
           columns={columns} 
           rows={rows} 
-          loading={fetching}
-          error={error}
+          loading={isLoading}
           components={{ LoadingOverlay: LinearProgress }}
         />
       </BasePanel>
